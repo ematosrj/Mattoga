061c40e9b51bbcc61683de98699101e77285c64b
En Matos <108830642+ematosrj@users.noreply.github.com>
2022-07-14 13:17:12 -0300

    Traduzi o texto

--- index.php
+++ index.php
@@ -1,17 +1,6 @@
 <?php
 
-	echo "Hello World!";
+	echo "Hello mundo!";
 
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Treinamento git atualizado11111111111</title>
-</head>
-<body>
-
-</body>
-</html>
