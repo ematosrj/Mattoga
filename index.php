3b58a4266c93809d0d8443aa91ba7a8d965a0e27
e.matos_rj@hotmail.com <SD PM MATOS DMT@DMT-SDS-MATOS-DSK.dmt.pm.ba.gov.br>
2022-07-14 10:40:36 -0300

    alteração para atualizado 10:40

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Treinamento git</title>
+	<title>Treinamento git atualizado11111111111</title>
 </head>
 <body>
 
