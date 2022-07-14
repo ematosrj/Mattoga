3b6a0570fb005fec186b410d37d713887efff061
e.matos_rj@hotmail.com <SD PM MATOS DMT@DMT-SDS-MATOS-DSK.dmt.pm.ba.gov.br>
2022-07-14 14:36:53 -0300

    retirada git linha um antes do php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 	echo "Ola mundo!";
-    echo "metodos de entrada git";
-    echo "git pull trazer para a maquina local";
-    echo "git push empurrar para o servidor gitlab";
-    echo "git add . adicionar";
+    echo "metodos de entrada git"
+    echo "git pull trazer para a maquina local"
+    echo "git push empurrar para o servidor gitlab"
+    echo "git add . adicionar"
 ?>
