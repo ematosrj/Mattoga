ba077ece50a939a0ecd211989dac2db687a77d31
e.matos_rj@hotmail.com <SD PM MATOS DMT@DMT-SDS-MATOS-DSK.dmt.pm.ba.gov.br>
2022-07-14 13:49:49 -0300

    adicionei 5 echo com siglas git

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
 	echo "Ola mundo!";
+    echo "metodos de entrada git"
+    echo ""
+    echo "git pull trazer para a maquina local"
+    echo ""
+    echo "git push empurrar para o servidor gitlab" 
 ?>
 
