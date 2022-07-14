f8005f89265d73db26161ee1a55f6476ead725dd
e.matos_rj@hotmail.com <SD PM MATOS DMT@DMT-SDS-MATOS-DSK.dmt.pm.ba.gov.br>
2022-07-14 13:23:50 -0300

    Add html

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php
 
-	echo "Hello mundo!";
+	echo "Hello World!";
 
 ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Ola mundo!!</title>
+</head>
+<body>
+
+</body>
+</html>
