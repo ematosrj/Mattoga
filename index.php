22f8ba71171aadf603c5c82f5ecce6ff2396e18a
En Matos <108830642+ematosrj@users.noreply.github.com>
2022-07-14 14:00:57 -0300

    Retirada do echos vazios

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php
 	echo "Ola mundo!";
     echo "metodos de entrada git"
-    echo ""
     echo "git pull trazer para a maquina local"
-    echo ""
     echo "git push empurrar para o servidor gitlab" 
 ?>
 
