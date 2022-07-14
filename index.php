6f3b859ba2b452d4701681f8ff74a09d12fc4a44
e.matos_rj@hotmail.com <SD PM MATOS DMT@DMT-SDS-MATOS-DSK.dmt.pm.ba.gov.br>
2022-07-14 09:52:59 -0300

    inseri codigo html

--- index.php
+++ index.php
@@ -4,3 +4,14 @@
 
 ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Treinamento git</title>
+</head>
+<body>
+
+</body>
+</html>
