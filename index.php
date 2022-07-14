6da78b94f17383dfb56f4551af71d3de9097831e
e.matos_rj@hotmail.com <SD PM MATOS DMT@DMT-SDS-MATOS-DSK.dmt.pm.ba.gov.br>
2022-07-14 13:44:03 -0300

    Retirada html

--- index.php
+++ index.php
@@ -1,17 +1,4 @@
 <?php
-
-	echo "Hello World!";
-
+	echo "Ola mundo!";
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Ola mundo!!</title>
-</head>
-<body>
-
-</body>
-</html>
