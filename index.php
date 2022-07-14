07be73997ffa28cd10d1eca7d070431f95557f9a
e.matos_rj@hotmail.com <SD PM MATOS DMT@DMT-SDS-MATOS-DSK.dmt.pm.ba.gov.br>
2022-07-14 14:22:06 -0300

    atualizacao1422

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
-    echo "Ola mundo!";
-    echo "metodos de entrada git";
-    echo "git pull trazer para a maquina local";
-    echo "git push empurrar para o servidor gitlab";
+	echo "Ola mundo234!";
+    echo "metodos de entrada git"
+    echo "git pull trazer para a maquina local"
+    echo "git push empurrar para o servidor gitlab" 
+    echo "git add . adicionar"
 ?>
 
