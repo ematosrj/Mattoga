7f2a8e712a7dc1a093b3dc49c09219c043469ef5
En Matos <108830642+ematosrj@users.noreply.github.com>
2022-07-14 14:06:22 -0300

    Alinhando o primeiro echo (linha 2)

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-	echo "Ola mundo!";
+    echo "Ola mundo!";
     echo "metodos de entrada git"
     echo "git pull trazer para a maquina local"
     echo "git push empurrar para o servidor gitlab" 
